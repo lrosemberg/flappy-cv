86dee41d79785d70e6a15920fb75a5a1deaa4d78
Leandro Rosemberg <leandro.rosemberg@buser.com.br>
2023-06-27 19:11:48 -0300

    Ident using 2 spaces

--- flappy.js
+++ flappy.js
@@ -1,27 +1,27 @@
 const board = {
-    width: 800,
-    height: 600,
-    canvas: undefined,
-    ctx: undefined
+  width: 800,
+  height: 600,
+  canvas: undefined,
+  ctx: undefined
 }
 
 const bird = {
-    width: 20,
-    height: 20,
-    x: 10,
-    y: (board.height / 2) - 10
+  width: 20,
+  height: 20,
+  x: 10,
+  y: (board.height / 2) - 10
 }
 
 
 window.onload = function() {
-    board.canvas = document.getElementById('canvas')
-    board.canvas.height = board.height
-    board.canvas.width = board.width
-    board.ctx = board.canvas.getContext('2d')
+  board.canvas = document.getElementById('canvas')
+  board.canvas.height = board.height
+  board.canvas.width = board.width
+  board.ctx = board.canvas.getContext('2d')
 
-    board.ctx.fillStyle = '#eee'
-    board.ctx.fillRect(0, 0, board.width, board.height)
+  board.ctx.fillStyle = '#eee'
+  board.ctx.fillRect(0, 0, board.width, board.height)
 
-    board.ctx.fillStyle = 'red'
-    board.ctx.fillRect(bird.x, bird.y, bird.width, bird.height)
+  board.ctx.fillStyle = 'red'
+  board.ctx.fillRect(bird.x, bird.y, bird.width, bird.height)
 }
